8beccf41081770189299384575f0235cdd87afc1
mahehasan <mahehasan@yahoo.com>
2022-06-05 11:14:02 +0600

    completed source

---
+++ README.md
@@ -0,0 +1,32 @@
+## Before jump into into the tutorial ,let me clear few things first.
+- I am using a Windows 10 machine with Xampp Server installed.
+- You need to install Composer- It is a Dependency Manager for PHP. You have to install it in your xampp path.
+##### So if you have already install all the required step up correctly, then start run this Laravel Project.
+- Pull project from git provider. https://github.com/mahehasan/calculatorbasedonemojis.git
+- Open the console and cd your project root directory
+- Run `composer install` Or `composer update`
+- Run `php artisan serve`
+
+## About calculator based on emojis
+
+#### Summary of requirements
+For our public facing application, we want to build a calculator based on emojis. As a proof 
+of concept, you need to build the app to support the initial set of calculations:
+- 👽 Addition (Alien U+1F47D)
+- 💀 Subtraction (Skull U+1F480)
+- 👻 Multiplication (Ghost U+1F47B)
+- 😱 Division (Scream U+1F631)
+
+#### Acceptance criteria
+##### When I view the website
+- And I want to calculate 1 - Alien - 1
+- Then I should receive the response 2 
+##### When I view the website
+- And I want to calculate 10 - Skull - 5
+- Then I should receive the response 5
+##### When I view the website
+- And I want to calculate 2 – Ghost - 2
+- Then I should receive the response 4
+##### When I view the website
+- And I want to calculate 2 – Scream - 2
+- Then I should receive the response 1
---
+++ app/Http/Controllers/CallCalculatorController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CallCalculatorController extends Controller
+{
+    public function processCall(Request $request){
+        $messages = [
+            'required' => 'The :attribute field is required/integer.',
+        ];
+        $validator = Validator::make($request->all(), [
+            'lho'=>'required|numeric',
+            'rho'=>'required|numeric'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+        $data = $this->exec($request);
+        return response()->json($data,200);
+	}
+
+    protected function exec($request)
+    {
+        try {
+            $lho = $request->input('lho');
+            $rho = $request->input('rho');
+            $op = $request->input('op');
+
+            $resultArr = array();
+            if($op=='add'){
+                $resultArr['calcResult'] = ($lho+$rho);
+                $resultArr['calcSummary'] = $lho . ' Alien 👽 ' . $rho . ' = ' . ($lho+$rho);
+            }
+            else if($op=='subtract'){
+                $resultArr['calcResult'] = ($lho-$rho);
+                $resultArr['calcSummary'] = $lho . ' Skull 💀 ' . $rho . ' = ' . ($lho-$rho);
+            }
+            else if($op=='multiply'){
+                $resultArr['calcResult'] = ($lho*$rho);
+                $resultArr['calcSummary'] = $lho . ' Ghost 👻 ' . $rho . ' = ' . ($lho*$rho);
+            }
+            else if($op=='divide'){
+                $resultArr['calcResult'] = ($lho/$rho);
+                $resultArr['calcSummary'] = $lho . ' Scream 😱 ' . $rho . ' = ' . ($lho/$rho);
+            }
+            else{
+                throw new InvalidArgumentException(sprintf('"%s" is an invalid operation.', str_replace('_', '', $request->input('op'))));
+            }
+            return $resultArr;
+        } catch (\Throwable $th) {
+            return $th->__toString();
+        }
+    }
+}
